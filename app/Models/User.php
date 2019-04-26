3dfd5dd2715de0aa5c5966b7a20bcbe535ba0d37
wuhonglie <2429986613@qq.com>
2019-04-26 03:26:12 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
     /**
      * The attributes that are mass assignable.
      *
