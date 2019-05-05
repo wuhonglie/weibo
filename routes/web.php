78ac7ed964f1049125936ccab4b5e6c051f2dfe2
wuhonglie <2429986613@qq.com>
2019-05-05 01:54:35 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password\reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+Route::get('users/{user}/followings','UserController@followings')->name('users.followings');
+Route::get('users/{user}/followers','UserController@followers')->name('users.followers');
